8b94d4a15f8a71d370e8a6bfdb1e1718da02bb86
agray998 <agray@qa.com>
2022-05-30 11:22:06 +0000

    updated build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+echo "Installing packages"
 sudo apt install python3 python3-pip python3-venv -y
+echo "Setting up venv"
 python3 -m venv venv
 source venv/bin/activate
+echo "Installing dependencies"
 pip3 install -r requirements.txt
+echo "Running unit tests"
 python3 -m pytest --cov=application --cov-report=html
